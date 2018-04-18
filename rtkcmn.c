e6af8e7b9a827df8aebcb6b7751f0f177e1e906f
Anissa Wong <aywong40@gmail.com>
2018-04-18 14:23:51 -0700

    Update rtkcmn.c

--- rtkcmn.c
+++ rtkcmn.c
@@ -135,39 +135,98 @@ extern double norm(const double *a, int n)
 {
     return sqrt(dot(a,a,n));
 }
-/* multiply matrix (wrapper of blas dgemm) 870-------------------------------------
-* multiply matrix by matrix (C=alpha*A*B+beta*C)
-* args   : char   *tr       I  transpose flags ("N":normal,"T":transpose)
-*          int    n,k,m     I  size of (transposed) matrix A,B
-*          double alpha     I  alpha
-*          double *A,*B     I  (transposed) matrix A (n x m), B (m x k)
-*          double beta      I  beta
-*          double *C        IO matrix C (n x k)
-* return : none
-*-----------------------------------------------------------------------------*/
 extern void matmul(const char *tr, int n, int k, int m, double alpha,
                    const double *A, const double *B, double beta, double *C)
 {
-    int lda=tr[0]=='T'?m:n,ldb=tr[1]=='T'?k:m;
+    double d;
+    int i,j,x,f=tr[0]=='N'?(tr[1]=='N'?1:2):(tr[1]=='N'?3:4);
     
-    dgemm_((char *)tr,(char *)tr+1,&n,&k,&m,&alpha,(double *)A,&lda,(double *)B,
-           &ldb,&beta,C,&n);
+    for (i=0;i<n;i++) for (j=0;j<k;j++) {
+        d=0.0;
+        switch (f) {
+            case 1: for (x=0;x<m;x++) d+=A[i+x*n]*B[x+j*m]; break;
+            case 2: for (x=0;x<m;x++) d+=A[i+x*n]*B[j+x*k]; break;
+            case 3: for (x=0;x<m;x++) d+=A[x+i*m]*B[x+j*m]; break;
+            case 4: for (x=0;x<m;x++) d+=A[x+i*m]*B[j+x*k]; break;
+        }
+        if (beta==0.0) C[i+j*n]=alpha*d; else C[i+j*n]=alpha*d+beta*C[i+j*n];
+    }
 }
-/* inverse of matrix 884-----------------------------------------------------------
-* inverse of matrix (A=A^-1)
-* args   : double *A        IO  matrix (n x n)
-*          int    n         I   size of matrix A
-* return : status (0:ok,0>:error)
-*-----------------------------------------------------------------------------*/
+/* LU decomposition ----------------------------------------------------------*/
+static int ludcmp(double *A, int n, int *indx, double *d)
+{
+    double big,s,tmp,*vv=mat(n,1);
+    int i,imax=0,j,k;
+    
+    *d=1.0;
+    for (i=0;i<n;i++) {
+        big=0.0; for (j=0;j<n;j++) if ((tmp=fabs(A[i+j*n]))>big) big=tmp;
+        if (big>0.0) vv[i]=1.0/big; else {free(vv); return -1;}
+    }
+    for (j=0;j<n;j++) {
+        for (i=0;i<j;i++) {
+            s=A[i+j*n]; for (k=0;k<i;k++) s-=A[i+k*n]*A[k+j*n]; A[i+j*n]=s;
+        }
+        big=0.0;
+        for (i=j;i<n;i++) {
+            s=A[i+j*n]; for (k=0;k<j;k++) s-=A[i+k*n]*A[k+j*n]; A[i+j*n]=s;
+            if ((tmp=vv[i]*fabs(s))>=big) {big=tmp; imax=i;}
+        }
+        if (j!=imax) {
+            for (k=0;k<n;k++) {
+                tmp=A[imax+k*n]; A[imax+k*n]=A[j+k*n]; A[j+k*n]=tmp;
+            }
+            *d=-(*d); vv[imax]=vv[j];
+        }
+        indx[j]=imax;
+        if (A[j+j*n]==0.0) {free(vv); return -1;}
+        if (j!=n-1) {
+            tmp=1.0/A[j+j*n]; for (i=j+1;i<n;i++) A[i+j*n]*=tmp;
+        }
+    }
+    free(vv);
+    return 0;
+}
+/* LU back-substitution ------------------------------------------------------*/
+static void lubksb(const double *A, int n, const int *indx, double *b)
+{
+    double s;
+    int i,ii=-1,ip,j;
+    
+    for (i=0;i<n;i++) {
+        ip=indx[i]; s=b[ip]; b[ip]=b[i];
+        if (ii>=0) for (j=ii;j<i;j++) s-=A[i+j*n]*b[j]; else if (s) ii=i;
+        b[i]=s;
+    }
+    for (i=n-1;i>=0;i--) {
+        s=b[i]; for (j=i+1;j<n;j++) s-=A[i+j*n]*b[j]; b[i]=s/A[i+i*n];
+    }
+}
+/* inverse of matrix ---------------------------------------------------------*/
 extern int matinv(double *A, int n)
 {
-    double *work;
-    int info,lwork=n*16,*ipiv=imat(n,1);
+    double d,*B;
+    int i,j,*indx;
     
-    work=mat(lwork,1);
-    dgetrf_(&n,&n,A,&n,ipiv,&info);
-    if (!info) dgetri_(&n,A,&n,ipiv,work,&lwork,&info);
-    free(ipiv); free(work);
+    indx=imat(n,1); B=mat(n,n); matcpy(B,A,n,n);
+    if (ludcmp(B,n,indx,&d)) {free(indx); free(B); return -1;}
+    for (j=0;j<n;j++) {
+        for (i=0;i<n;i++) A[i+j*n]=0.0; A[j+j*n]=1.0;
+        lubksb(B,n,indx,A+j*n);
+    }
+    free(indx); free(B);
+    return 0;
+}
+/* solve linear equation -----------------------------------------------------*/
+extern int solve(const char *tr, const double *A, const double *Y, int n,
+                 int m, double *X)
+{
+    double *B=mat(n,n);
+    int info;
+    
+    matcpy(B,A,n,n);
+    if (!(info=matinv(B,n))) matmul(tr[0]=='N'?"NN":"TN",n,m,n,1.0,B,Y,0.0,X);
+    free(B);
     return info;
 }
 /* least square estimation 1020-----------------------------------------------------
