86ff7c95b0be1303e8a90bfa4b27c05b8d605dc3
Anissa Wong <aywong40@gmail.com>
2018-04-18 14:20:15 -0700

    Update main.c

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 int main(int argc, char **argv)
 {
 int i,j,ret;
-ret=estpos(ts,te,tint,0.0,&prcopt,&solopt,&filopt,infile,n,outfile,"","");
-ret=postpos(ts,te,tint,0.0,&prcopt,&solopt,&filopt,infile,n,outfile,"","");
+ret=estpos(obs,n,rs,dts,nav,&opt_,sol,azel,vsat,resp,msg);
+/* ret=postpos(ts,te,tint,0.0,&prcopt,&solopt,&filopt,infile,n,outfile,"",""); */
     
     if (!ret) fprintf(stderr,"%40s\r","");
     return ret;
