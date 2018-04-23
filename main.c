6d83f0f726a21b9d9055dadd66de36f7f035d19c
Anissa Wong <aywong40@gmail.com>
2018-04-23 13:56:42 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 /* main function file  ----------------------------------------------------
 *-----------------------------------------------------------------------------*/
-
+#include <stdarg.h>
+#include "rtklib.h"
 int main(int argc, char **argv)
 {
-int i,j,ret;
-ret=estpos(obs,n,rs,dts,nav,&opt_,sol,azel,vsat,resp,msg);
-/* ret=postpos(ts,te,tint,0.0,&prcopt,&solopt,&filopt,infile,n,outfile,"",""); */
+
+    prcopt_t opt_=opt_;
+    double *rs,*dts,*var,*azel_,*resp;
+    int i,n,stat,vsat,svh;
+
+
+
+
+     rs=mat(6,n); dts=mat(2,n); var=mat(1,n); azel_=zeros(2,n); resp=mat(1,n);
+     if (n<=0) {
+
+        return -2;
+    }
+    stat=estpos(n,rs,dts,var,svh,opt_,azel_,vsat,resp);
+    if (!stat) fprintf(stderr,"%40s\r","");
+    return stat;
+}   
+     
+   
     
-    if (!ret) fprintf(stderr,"%40s\r","");
-    return ret;
-}
+  
