387320235daefc83235f22285f2a27335ac6d54f
Anissa Wong <aywong40@gmail.com>
2018-07-10 16:14:01 -0700

    Update satpos.c

--- satpos.c
+++ satpos.c
@@ -1,1 +1,120 @@
 /* estimate satellite postition */
+/* based on ephemeris.c in rtklib by tomojitakasu */
+/* inputs:
+* transmit time 
+* prn list
+* ephemerices of satellites */
+/* outputs:
+* satposition in ECEF X,Y,Z
+* sat clock correction */
+/* satellite position and clock ------------------------------------------------
+* compute satellite position, velocity and clock
+* args   : gtime_t time     I   time (gpst)
+*          gtime_t teph     I   time to select ephemeris (gpst)
+*          int    sat       I   satellite number
+*          nav_t  *nav      I   navigation data
+*          int    ephopt    I   ephemeris option (EPHOPT_???)
+*          double *rs       O   sat position and velocity (ecef)
+*                               {x,y,z,vx,vy,vz} (m|m/s)
+*          double *dts      O   sat clock {bias,drift} (s|s/s)
+*          double *var      O   sat position and clock error variance (m^2)
+*          int    *svh      O   sat health flag (-1:correction not available)
+* return : status (1:ok,0:error)
+* notes  : satellite position is referenced to antenna phase center
+*          satellite clock does not include code bias correction (tgd or bgd)
+*-----------------------------------------------------------------------------*/
+extern int satpos(gtime_t time, gtime_t teph, int sat, int ephopt,
+                  const nav_t *nav, double *rs, double *dts, double *var,
+                  int *svh)
+{
+    trace(4,"satpos  : time=%s sat=%2d ephopt=%d\n",time_str(time,3),sat,ephopt);
+    
+    *svh=0;
+    
+    switch (ephopt) {
+        case EPHOPT_BRDC  : return ephpos     (time,teph,sat,nav,-1,rs,dts,var,svh);
+        case EPHOPT_SBAS  : return satpos_sbas(time,teph,sat,nav,   rs,dts,var,svh);
+        case EPHOPT_SSRAPC: return satpos_ssr (time,teph,sat,nav, 0,rs,dts,var,svh);
+        case EPHOPT_SSRCOM: return satpos_ssr (time,teph,sat,nav, 1,rs,dts,var,svh);
+        case EPHOPT_PREC  :
+            if (!peph2pos(time,sat,nav,1,rs,dts,var)) break; else return 1;
+        case EPHOPT_LEX   :
+            if (!lexeph2pos(time,sat,nav,rs,dts,var)) break; else return 1;
+    }
+    *svh=-1;
+    return 0;
+}
+/* satellite positions and clocks ----------------------------------------------
+* compute satellite positions, velocities and clocks
+* args   : gtime_t teph     I   time to select ephemeris (gpst)
+*          obsd_t *obs      I   observation data
+*          int    n         I   number of observation data
+*          nav_t  *nav      I   navigation data
+*          int    ephopt    I   ephemeris option (EPHOPT_???)
+*          double *rs       O   satellite positions and velocities (ecef)
+*          double *dts      O   satellite clocks
+*          double *var      O   sat position and clock error variances (m^2)
+*          int    *svh      O   sat health flag (-1:correction not available)
+* return : none
+* notes  : rs [(0:2)+i*6]= obs[i] sat position {x,y,z} (m)
+*          rs [(3:5)+i*6]= obs[i] sat velocity {vx,vy,vz} (m/s)
+*          dts[(0:1)+i*2]= obs[i] sat clock {bias,drift} (s|s/s)
+*          var[i]        = obs[i] sat position and clock error variance (m^2)
+*          svh[i]        = obs[i] sat health flag
+*          if no navigation data, set 0 to rs[], dts[], var[] and svh[]
+*          satellite position and clock are values at signal transmission time
+*          satellite position is referenced to antenna phase center
+*          satellite clock does not include code bias correction (tgd or bgd)
+*          any pseudorange and broadcast ephemeris are always needed to get
+*          signal transmission time
+*-----------------------------------------------------------------------------*/
+extern void satposs(gtime_t teph, const obsd_t *obs, int n, const nav_t *nav,
+                    int ephopt, double *rs, double *dts, double *var, int *svh)
+{
+    gtime_t time[MAXOBS]={{0}};
+    double dt,pr;
+    int i,j;
+    
+    trace(3,"satposs : teph=%s n=%d ephopt=%d\n",time_str(teph,3),n,ephopt);
+    
+    for (i=0;i<n&&i<MAXOBS;i++) {
+        for (j=0;j<6;j++) rs [j+i*6]=0.0;
+        for (j=0;j<2;j++) dts[j+i*2]=0.0;
+        var[i]=0.0; svh[i]=0;
+        
+        /* search any psuedorange */
+        for (j=0,pr=0.0;j<NFREQ;j++) if ((pr=obs[i].P[j])!=0.0) break;
+        
+        if (j>=NFREQ) {
+            trace(2,"no pseudorange %s sat=%2d\n",time_str(obs[i].time,3),obs[i].sat);
+            continue;
+        }
+        /* transmission time by satellite clock */
+        time[i]=timeadd(obs[i].time,-pr/CLIGHT);
+        
+        /* satellite clock bias by broadcast ephemeris */
+        if (!ephclk(time[i],teph,obs[i].sat,nav,&dt)) {
+            trace(2,"no broadcast clock %s sat=%2d\n",time_str(time[i],3),obs[i].sat);
+            continue;
+        }
+        time[i]=timeadd(time[i],-dt);
+        
+        /* satellite position and clock at transmission time */
+        if (!satpos(time[i],teph,obs[i].sat,ephopt,nav,rs+i*6,dts+i*2,var+i,
+                    svh+i)) {
+            trace(2,"no ephemeris %s sat=%2d\n",time_str(time[i],3),obs[i].sat);
+            continue;
+        }
+        /* if no precise clock available, use broadcast clock instead */
+        if (dts[i*2]==0.0) {
+            if (!ephclk(time[i],teph,obs[i].sat,nav,dts+i*2)) continue;
+            dts[1+i*2]=0.0;
+            *var=SQR(STD_BRDCCLK);
+        }
+    }
+    for (i=0;i<n&&i<MAXOBS;i++) {
+        trace(4,"%s sat=%2d rs=%13.3f %13.3f %13.3f dts=%12.3f var=%7.3f svh=%02X\n",
+              time_str(time[i],6),obs[i].sat,rs[i*6],rs[1+i*6],rs[2+i*6],
+              dts[i*2]*1E9,var[i],svh[i]);
+    }
+}
